c8a225d33d35d7b347bed391cec65ffdc81acf1f
ZlWeng1993 <zlweng1993@163.com>
2021-11-30 19:31:03 +0100

    add annotation of code

--- cell_is/data/LIVE_Cell_Dataset.py
+++ cell_is/data/LIVE_Cell_Dataset.py
@@ -23,8 +23,20 @@ from pycocotools import mask as COCOmask
 
 class LIVE_Cell_Dataset(COCO):
     def __init__(self, json_File_Path = None):
-        self.dataset,self.anns,self.cats,self.imgs = dict(),dict(),dict(),dict()
-        self.imgToAnns, self.catToImgs = defaultdict(list), defaultdict(list)
+        """
+        Class-Params:
+            self.dataset(dict): all the data in json-file
+            self.anns(dict): {keys -> ann_id : values -> annotation}
+            self.imgs(dict): {keys -> image_id : values -> info of image}
+            self.types(list): names of celltypes
+            self.imgToAnns(dict): {keys -> image_id : values -> anns}
+            self.typeToImgs(dict): {keys -> CellType : values -> list of images_info}
+        Other functions of COCO:
+            in pycocotools/coco.py
+        """
+        self.dataset,self.anns,self.imgs = dict(),dict(),dict()
+        self.types = []
+        self.imgToAnns, self.typeToImgs = defaultdict(list), defaultdict(list)
         if not json_File_Path == None:
             print("loading json-File into memory...")
             tic = time.time()
@@ -33,13 +45,14 @@ class LIVE_Cell_Dataset(COCO):
             print('Done (t={:0.2f}s)'.format(time.time()- tic))
             self.dataset = dataset
             self._createIndex()
+            self.types = cfg.TRAIN.CELL_TYPE
         
     def _createIndex(self):
         print('creating index...')
         anns, imgs = {}, {}
-        imgToAnns, catToImgs = defaultdict(list), defaultdict(list)
-        for cat in cfg.TRAIN.CELL_TYPE:
-            catToImgs[cat] = []
+        imgToAnns, typeToImgs = defaultdict(list), defaultdict(list)
+        for celltype in cfg.TRAIN.CELL_TYPE:
+            typeToImgs[celltype] = []
         if 'annotations' in self.dataset:
             for ann in self.dataset['annotations']:
                 imgToAnns[self.dataset['annotations'][ann]['image_id']].append(self.dataset['annotations'][ann])
@@ -48,19 +61,27 @@ class LIVE_Cell_Dataset(COCO):
         if 'images' in self.dataset:
             for img in self.dataset['images']:
                 imgs[img['id']] = img
-                for cat in cfg.TRAIN.CELL_TYPE:
-                    if cat in img['file_name']:
-                        catToImgs[cat].append(img)
+                for celltype in cfg.TRAIN.CELL_TYPE:
+                    if celltype in img['file_name']:
+                        typeToImgs[celltype].append(img)
                         break
             
         print('index created!')
         
         self.anns = anns
         self.imgs = imgs
-        self.catToImgs = catToImgs
+        self.typeToImgs = typeToImgs
         self.imgToAnns = imgToAnns
         
     def Img_Mask(self, img_id = None, show = False):
+        """
+            Show the mask of image, that mean, use all of the Anns of one image
+            Params:
+                img_id(int): the image_id
+                show(boolean): True: show the image with mask
+            Return:
+                ImgMask(ndarray):shape is the same as image, the values are 0 or 1
+        """
         if img_id == None:
             return 0
         anns = self.imgToAnns[img_id]
@@ -77,6 +98,13 @@ class LIVE_Cell_Dataset(COCO):
         return ImgMask
     
     def getImgPath(self, img_ids = []):
+        """
+        get the path of images
+        Params:
+            img_ids(list): list of image_ids
+        Return:
+            Path(list): list of the path of images
+        """
         if len(img_ids) == 0:
             return 0
         Path = []
@@ -87,6 +115,13 @@ class LIVE_Cell_Dataset(COCO):
         return Path
             
     def getbbox(self, img_id = None):
+        """
+        get the bbox of one image
+        Param:
+            img_id(int): image_id
+        Return:
+            Bbox(list): list of all the bboxes in one image
+        """
         if img_id == None:
             return 0
         Bbox_lst = []
@@ -94,11 +129,8 @@ class LIVE_Cell_Dataset(COCO):
         for ann in anns:
             bbox = ann['bbox']
             Bbox_lst.append(bbox)
-        return Bbox
+        return Bbox_lst
             
         
 if __name__ == "__main__":
     live_celldata = LIVE_Cell_Dataset(cfg.PATH.LIVE_TRAIN_JSON)
-    
-    
-
